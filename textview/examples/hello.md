7eb4288497e220f77fbb66538c9880a7e577049b
Oliver N <olvrng@gmail.com>
2024-02-17 21:52:16 +0700

    custom titlebar draggable, cmd+w to close window

--- textview/examples/hello.md
+++ textview/examples/hello.md
@@ -4,8 +4,10 @@
 
 - [x] Create a new app
 - [x] Create simple layout
+- [x] Custom titlebar and draggable
+- [x] Cmd+W to close window
+- [ ] Click icon on dock to re-open
 - [ ] Clickable area
-- [ ] Keyboard event
-- [ ] Custom titlebar and draggable
-- [ ] Cmd+Q to quit
+- [ ] Keyboard event (left, right, enter)
+- [ ] Open new child window
 - [ ] Open file dialog
--- textview/examples/hello.rs
+++ textview/examples/hello.rs
@@ -1,32 +1,53 @@
 use floem::{
+    keyboard::{self, Key, ModifiersState},
+    menu::{Menu, MenuItem},
     peniko::Color,
     style::Position,
     unit::{Px, PxPctAuto},
     view::View,
-    views::{container, empty, label, stack, text, v_stack, Decorators, Empty},
-    window::WindowConfig,
+    views::{container, drag_window_area, empty, label, stack, text, v_stack, Decorators, Empty},
+    window::{WindowConfig, WindowId},
     Application,
 };
+use smol_str::{self, ToSmolStr};
+
+struct WindowInfo {
+    window_id: WindowId,
+}
 
 fn main() {
     let opts = WindowConfig::default()
         .show_titlebar(false)
         .with_transparent(true);
     Application::new()
-        .window(move |_| app_view(), Some(opts))
+        .window(
+            move |window_id| {
+                let window_info = WindowInfo { window_id };
+                let app_view = app_view().window_menu(|| app_menu());
+                let app_view = app_keyboard(app_view, window_info);
+                app_view
+            },
+            Some(opts),
+        )
         .run()
 }
 
 fn app_view() -> impl View {
     v_stack((
-        container(text("Title")).style(|s| {
+        stack((drag_window_area(container(text("Title")).style(|s| {
             s.width_full()
                 .background(Color::rgb8(255, 255, 180))
                 .flex_row()
                 .justify_center()
                 .items_center()
-                .height(Px(40.0))
-        }),
+        }))
+        .style(|s| {
+            s.height_full()
+                .flex_basis(0.0)
+                .flex_grow(1.0)
+                .background(Color::rgb8(100, 100, 100))
+        }),))
+        .style(|s| s.height(Px(40.0))),
         stack((
             left_sidebar().style(|s| s.width(Px(100.0))),
             center_pane().style(|s| s.width_full().background(Color::rgb8(200, 255, 200))),
@@ -44,12 +65,13 @@ fn app_view() -> impl View {
                     .width(Px(100.0))
                     .items_start()
             }),
-            container(text("open a file")).style(|s| {
+            container(text("click to choose a file")).style(|s| {
                 s.width_full()
                     .justify_center()
                     .items_center()
                     .background(Color::rgb8(220, 255, 220))
-                    .font_size(32.0)
+                    .font_size(14.0)
+                    .font_family("Arial".to_string())
             }),
             container(empty().style(|s| {
                 s.width(Px(20.0))
@@ -104,4 +126,31 @@ fn center_pane() -> impl View {
         .style(|s| s.justify_center().items_center()),
     )
     .style(|s| s.justify_center().items_center())
+}
+
+// NOTE: didn't work on macos
+fn app_menu() -> Menu {
+    Menu::new("Main Menu").entry(
+        Menu::new("Hello")
+            .entry(MenuItem::new("The about menu item"))
+            .separator()
+            .entry(MenuItem::new("The quit menu item")),
+    )
+}
+
+fn app_keyboard<V>(view: V, window_info: WindowInfo) -> V
+where
+    V: View,
+{
+    view.keyboard_navigatable().on_key_down(
+        Key::Character("w".to_smolstr()),
+        key_super(),
+        move |_| floem::close_window(window_info.window_id),
+    )
+}
+
+fn key_super() -> ModifiersState {
+    let mut mods = ModifiersState::empty();
+    mods.set(ModifiersState::SUPER, true);
+    mods
 }
