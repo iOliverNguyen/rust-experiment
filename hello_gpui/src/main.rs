a24973d7260454b738daabb78d4dc7576e677aa8
Oliver N <olvrng@gmail.com>
2024-02-21 18:19:35 +0700

    gpui: left or right to change content, support click and keypress

--- hello_gpui/src/main.rs
+++ hello_gpui/src/main.rs
@@ -1,6 +1,72 @@
+use std::ops::Deref;
+
 use gpui::*;
 
 actions!(app, [Quit, CloseWindow, ChooseFile]);
+
+use once_cell::sync::Lazy;
+
+static KEY_LEFT: Lazy<Keystroke> = Lazy::new(|| Keystroke::parse("left").unwrap());
+static KEY_RIGHT: Lazy<Keystroke> = Lazy::new(|| Keystroke::parse("right").unwrap());
+static KEY_ENTER: Lazy<Keystroke> = Lazy::new(|| Keystroke::parse("enter").unwrap());
+static KEY_ESC: Lazy<Keystroke> = Lazy::new(|| Keystroke::parse("esc").unwrap());
+
+struct Planet {
+    name: String,
+    desc: String,
+}
+
+impl Planet {
+    fn new(name: &str, desc: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            desc: desc.to_string(),
+        }
+    }
+}
+
+struct ListPlanets {
+    items: Vec<Planet>,
+    default_index: usize,
+}
+
+impl Deref for ListPlanets {
+    type Target = [Planet];
+
+    fn deref(&self) -> &Self::Target {
+        &self.items
+    }
+}
+
+impl ListPlanets {
+    fn init() -> Self {
+        let planets = vec![
+Planet::new("Mercury", "Mercury is the smallest and closest planet to the Sun. It has a rocky surface marked with craters similar to the Moon. Because of its proximity to the Sun, it experiences extreme temperature variations"),
+Planet::new("Venus","Venus is similar in size and structure to Earth but is shrouded in a thick, toxic atmosphere that traps heat, leading to surface temperatures hot enough to melt lead. Its surface is volcanic and covered in sulfuric acid clouds."),
+Planet::new("Earth", "Earth is the only planet known to support life, with a diverse ecosystem. It has a unique atmosphere composed of nitrogen, oxygen, and other gases, liquid water on its surface, and a varied terrain including mountains, valleys, and oceans."),
+Planet::new("Mars", "Mars, known as the Red Planet due to its reddish appearance, has the tallest volcano and the deepest, longest canyon in the Solar System. It has water ice at its poles and evidence suggests it once had liquid water on its surface."),
+Planet::new("Jupiter", "Jupiter is the largest planet in the Solar System. A gas giant, it has a Great Red Spot, a giant storm that has raged for hundreds of years. Jupiter has a strong magnetic field and at least 79 moons, including the four large Galilean moons: Io, Europa, Ganymede, and Callisto."),
+Planet::new("Saturn", "Saturn is best known for its stunning ring system, made up of ice and rock particles. It is a gas giant like Jupiter, with a composition mainly of hydrogen and helium. Saturn has numerous moons, with Titan being the largest and having a thick atmosphere."),
+Planet::new("Uranus", "Uranus is unique for its tilted axis, which causes it to rotate on its side. It is an ice giant with a colder atmosphere containing water, ammonia, and methane. Uranus has a faint ring system and 27 known moons."),
+Planet::new("Neptune", "Neptune, an ice giant, is known for its deep blue color, caused by methane in its atmosphere. It has the strongest winds of any planet in the Solar System. Neptune has 14 known moons, with Triton being the largest and geologically active."),
+        ];
+        let default_index = planets
+            .iter()
+            .enumerate()
+            .find_map(|(idx, planet)| {
+                if &planet.name == "Earth" {
+                    Some(idx)
+                } else {
+                    None
+                }
+            })
+            .unwrap();
+        ListPlanets {
+            items: planets,
+            default_index,
+        }
+    }
+}
 
 fn main() {
     let app = App::new();
@@ -27,8 +93,10 @@ fn main() {
             }),
             ..WindowOptions::default()
         };
+
+        let list_planets = cx.new_model(|cx| ListPlanets::init());
         cx.open_window(opts, |cx| {
-            let app_view = cx.new_view(AppView::new);
+            let app_view = cx.new_view(|cx| AppView::new(cx, list_planets));
             cx.focus_view(&app_view);
             app_view
         });
@@ -71,25 +139,71 @@ fn action_choose_file(_: &ChooseFile, cx: &mut gpui::AppContext) {
 }
 
 struct AppView {
-    pub root_focus_handle: FocusHandle,
+    focus_handle: FocusHandle,
+
+    list_items: Model<ListPlanets>,
+    active_idx: usize,
 }
 
 impl AppView {
-    fn new(cx: &mut ViewContext<Self>) -> Self {
+    fn new(cx: &mut ViewContext<Self>, list: Model<ListPlanets>) -> Self {
         let focus_handle = cx.focus_handle();
         AppView {
-            root_focus_handle: focus_handle,
+            focus_handle,
+            active_idx: list.read_with(cx, |x, cx| x.default_index),
+            list_items: list,
         }
     }
 
+    fn bind_root_actions<T: StatefulInteractiveElement>(&self, root: T) -> T {
+        root.on_action(|_: &CloseWindow, cx| cx.remove_window())
+    }
+
+    fn is_left_available(&self, cx: &ViewContext<Self>) -> bool {
+        self.active_idx > 0
+    }
+
+    fn is_right_available(&self, cx: &ViewContext<Self>) -> bool {
+        self.active_idx < self.list_items.read(cx).len() - 1
+    }
+
     fn handle_keydown(&mut self, ev: &KeyDownEvent, cx: &mut ViewContext<Self>) {
-        dbg!(ev);
+        if let KeyDownEvent {
+            keystroke,
+            is_held: false,
+        } = ev
+        {
+            if *keystroke == *KEY_LEFT {
+                self.handle_left(cx);
+                cx.refresh();
+            }
+            if *keystroke == *KEY_RIGHT {
+                self.handle_right(cx);
+                cx.refresh();
+            }
+        };
+    }
+
+    fn handle_left(&mut self, cx: &mut ViewContext<Self>) {
+        if self.is_left_available(cx) {
+            self.active_idx -= 1;
+        }
+    }
+
+    fn handle_right(&mut self, cx: &mut ViewContext<Self>) {
+        if self.is_right_available(cx) {
+            self.active_idx += 1;
+        }
+    }
+
+    fn handle_center(&mut self, cx: &mut ViewContext<Self>) {
+        println!("center")
     }
 }
 
 impl FocusableView for AppView {
     fn focus_handle(&self, cx: &AppContext) -> FocusHandle {
-        self.root_focus_handle.clone()
+        self.focus_handle.clone()
     }
 }
 
@@ -97,8 +211,9 @@ impl Render for AppView {
     fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
         div()
             .id("root")
+            .apply(|x| self.bind_root_actions(x))
             .key_context("AppView")
-            .track_focus(&self.root_focus_handle)
+            .track_focus(&self.focus_handle)
             .on_action(|act: &CloseWindow, cx| cx.remove_window())
             .on_key_down(cx.listener(Self::handle_keydown))
             .size_full()
@@ -118,8 +233,10 @@ impl Render for AppView {
                     .flex()
                     .justify_center()
                     .items_center()
-                    .children([
+                    .child(
                         div()
+                            .id("left")
+                            .on_click(cx.listener(|this, _, cx| this.handle_left(cx)))
                             .flex_center() // use StyledExtension
                             .w(px(100.))
                             .h_full()
@@ -127,35 +244,60 @@ impl Render for AppView {
                             .flex()
                             .justify_center()
                             .items_center()
-                            .text_color(rgb(0xFF6600))
+                            .apply(|x| {
+                                if self.is_left_available(cx) {
+                                    x.cursor_pointer().text_color(rgb(0xFF6600))
+                                } else {
+                                    x.cursor_default()
+                                        .text_color(rgb(mix32(0xFF6600, 0xFFFFFF)))
+                                }
+                            })
                             .bg(rgb(0xFFDD66))
                             .child("◀"),
+                    )
+                    .child(
                         div()
+                            .id("center")
+                            .on_click(cx.listener(|this, _, cx| this.handle_center(cx)))
                             .size_full()
-                            .flex()
-                            .justify_center()
-                            .items_center()
+                            .flex_center()
                             .bg(rgb(0xAAFFAA))
                             .child(
-                                div().children([
-                                    div().text_size(px(100.)).child("center"),
+                                div().w_full().children([
+                                    div().w_full().flex_center().text_size(px(100.)).child(
+                                        match self.list_items.read(cx).get(self.active_idx) {
+                                            Some(item) => item.name.clone(),
+                                            None => String::new(),
+                                        },
+                                    ),
                                     div()
                                         .w_full()
-                                        .flex()
-                                        .justify_center()
+                                        .flex_center()
                                         .absolute()
-                                        .bottom(px(120.))
+                                        .bottom(px(140.))
                                         .child("click to open"),
                                 ]),
                             ),
-                        ext(div(), |x| x.flex_center()) // use ext()
+                    )
+                    .child(
+                        div()
+                            .apply(|x| x.flex_center())
+                            .id("right") // use ext()
+                            .on_click(cx.listener(|this, _, cx| this.handle_right(cx)))
                             .w(px(100.))
                             .h_full()
                             .text_size(px(100.))
-                            .text_color(rgb(0x0066FF))
+                            .apply(|x| {
+                                if self.is_right_available(cx) {
+                                    x.cursor_pointer().text_color(rgb(0x0066FF))
+                                } else {
+                                    x.cursor_default()
+                                        .text_color(rgb(mix32(0x0066FF, 0xFFFFFF)))
+                                }
+                            })
                             .bg(rgb(0x66DDFF))
                             .child("▶"),
-                    ]),
+                    ),
                 div().h(px(40.)).justify_center().items_center().children([
                     div(),
                     div()
@@ -184,15 +326,19 @@ impl Render for AppView {
     }
 }
 
-impl StyledExtension for Div {}
+impl<T: Styled> StyledExtension for T {}
+impl<T: IntoElement> ElementExtension for T {}
+
 trait StyledExtension: Styled {
     fn flex_center(self) -> Self {
         self.flex().justify_center().items_center()
     }
 }
 
-fn ext<T: IntoElement>(x: T, f: impl Fn(T) -> T) -> T {
-    f(x)
+trait ElementExtension: IntoElement {
+    fn apply(self, f: impl FnOnce(Self) -> Self) -> Self {
+        f(self)
+    }
 }
 
 fn mix32(a: u32, b: u32) -> u32 {
